d9e5e39a512acf22dc083564a16ba259ae49f8d1
Lydia Chan <chan.lydia528@gmail.com>
2019-12-17 01:26:23 -0800

    Added code for online learning using BHM
    This includes the BHM code and the code for creating a video of online learning using BHM. Sample data and output will be uploaded soon.

---
+++ BHM Online Learning/bhmtorch_cpu.py
@@ -0,0 +1,414 @@
+"""
+# 2D and 3D Bayesian Hilbert Maps with pytorch
+# Ransalu Senanayake
+"""
+import torch as pt
+import numpy as np
+from sklearn.metrics.pairwise import rbf_kernel
+import matplotlib.pyplot as pl
+from mpl_toolkits.mplot3d import Axes3D
+import time
+import sys
+import pandas as pd
+
+dtype = pt.float32
+device = pt.device("cpu")
+#device = pt.device("cuda:0") # Uncomment this to run on GPU
+
+#TODO: merege 2D and 3D classes into a single class
+#TODO: get rid of all numpy operations and test on a GPU
+#TODO: parallelizing the segmentations
+#TODO: efficient querying
+#TODO: batch training
+#TODO: re-using parameters for moving vehicles
+class BHM2D_PYTORCH():
+    def __init__(self, gamma=0.05, grid=None, cell_resolution=(5, 5), cell_max_min=None, X=None, nIter=0, mu_sig=None):
+        """
+        :param gamma: RBF bandwidth
+        :param grid: if there are prespecified locations to hinge the RBF
+        :param cell_resolution: if 'grid' is 'None', resolution to hinge RBFs
+        :param cell_max_min: if 'grid' is 'None', realm of the RBF field
+        :param X: a sample of lidar locations to use when both 'grid' and 'cell_max_min' are 'None'
+        """
+        self.gamma = gamma
+        if grid is not None:
+            self.grid = grid
+        else:
+            self.grid = self.__calc_grid_auto(cell_resolution, cell_max_min, X)
+        self.nIter = nIter
+        print(' Number of hinge points={}'.format(self.grid.shape[0]))
+
+        #ADDED
+        if mu_sig is not None:
+            self.mu = pt.tensor(mu_sig[:,0], dtype=pt.float32)
+            self.sig = pt.tensor(mu_sig[:,1], dtype=pt.float32)
+
+    def updateGrid(grid):
+        self.grid = grid
+
+    def updateMuSig(mu_sig):
+        self.mu = pt.tensor(mu_sig[:,0], dtype=pt.float32)
+        self.sig = pt.tensor(mu_sig[:,1], dtype=pt.float32)
+
+    def __calc_grid_auto(self, cell_resolution, max_min, X):
+        """
+        :param X: a sample of lidar locations
+        :param cell_resolution: resolution to hinge RBFs as (x_resolution, y_resolution)
+        :param max_min: realm of the RBF field as (x_min, x_max, y_min, y_max)
+        :return: numpy array of size (# of RNFs, 2) with grid locations
+        """
+        # X = X.numpy()
+
+        if max_min is None:
+            # if 'max_min' is not given, make a boundarary based on X
+            # assume 'X' contains samples from the entire area
+            expansion_coef = 1.2
+            x_min, x_max = expansion_coef*X[:, 0].min(), expansion_coef*X[:, 0].max()
+            y_min, y_max = expansion_coef*X[:, 1].min(), expansion_coef*X[:, 1].max()
+        else:
+            x_min, x_max = max_min[0], max_min[1]
+            y_min, y_max = max_min[2], max_min[3]
+
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, cell_resolution[0]), \
+                             np.arange(y_min, y_max, cell_resolution[1]))
+        grid = np.hstack((xx.ravel()[:, np.newaxis], yy.ravel()[:, np.newaxis]))
+
+        return pt.tensor(grid)
+
+    def __sparse_features(self, X):
+        """
+        :param X: inputs of size (N,2)
+        :return: hinged features with intercept of size (N, # of features + 1)
+        """
+        rbf_features = rbf_kernel(X, self.grid, gamma=self.gamma)
+        # COMMENTED OUT BIAS TERM
+        # rbf_features = np.hstack((np.ones(X.shape[0])[:, np.newaxis], rbf_features))
+        return pt.tensor(rbf_features, dtype=pt.float32)
+
+    def __calc_posterior(self, X, y, epsilon, mu0, sig0):
+        """
+        :param X: input features
+        :param y: labels
+        :param epsilon: per dimension local linear parameter
+        :param mu0: mean
+        :param sig0: variance
+        :return: new_mean, new_varaiance
+        """
+        logit_inv = pt.sigmoid(epsilon)
+        lam = 0.5 / epsilon * (logit_inv - 0.5)
+        sig = 1/(1/sig0 + 2*pt.sum( (X.t()**2)*lam, dim=1))
+        mu = sig*(mu0/sig0 + pt.mm(X.t(), y - 0.5).squeeze())
+        return mu, sig
+
+    def fit(self, X, y):
+        """
+        :param X: raw data
+        :param y: labels
+        """
+        X = self.__sparse_features(X)
+        N, D = X.shape[0], X.shape[1]
+
+        self.epsilon = pt.ones(N, dtype=pt.float32)
+        if not hasattr(self, 'mu'):
+            self.mu = pt.zeros(D, dtype=pt.float32)
+            self.sig = 10000 * pt.ones(D, dtype=pt.float32)
+
+        for i in range(self.nIter):
+            print("  Parameter estimation: iter={}".format(i))
+
+            # E-step
+            self.mu, self.sig = self.__calc_posterior(X, y, self.epsilon, self.mu, self.sig)
+
+            # M-step
+            self.epsilon = pt.sqrt(pt.sum((X**2)*self.sig, dim=1) + (X.mm(self.mu.reshape(-1, 1))**2).squeeze())
+
+        # print(self.mu)
+
+        return self.mu, self.sig
+
+
+    def predict(self, Xq):
+        """
+        :param Xq: raw inquery points
+        :return: mean occupancy (Lapalce approximation)
+        """
+        Xq = self.__sparse_features(Xq)
+
+        mu_a = Xq.mm(self.mu.reshape(-1, 1)).squeeze()
+        sig2_inv_a = pt.sum((Xq ** 2) * self.sig, dim=1)
+        k = 1.0 / pt.sqrt(1 + np.pi * sig2_inv_a / 8)
+
+        return pt.sigmoid(k*mu_a)
+
+    def predictSampling(self, Xq, nSamples=50):
+        """
+        :param Xq: raw inquery points
+        :param nSamples: number of samples to take the average over
+        :return: sample mean and standard deviation of occupancy
+        """
+        Xq = self.__sparse_features(Xq)
+
+        qw = pt.distributions.MultivariateNormal(self.mu, pt.diag(self.sig))
+        w = qw.sample((nSamples,)).t()
+
+        mu_a = Xq.mm(w).squeeze()
+        probs = pt.sigmoid(mu_a)
+
+        mean = pt.std(probs, dim=1).squeeze()
+        std = pt.std(probs, dim=1).squeeze()
+
+        return mean, std
+
+
+class BHM3D_PYTORCH():
+    def __init__(self, gamma=0.05, grid=None, cell_resolution=(5, 5), cell_max_min=None, X=None, nIter=2, mu_sig=None):
+        """
+        :param gamma: RBF bandwidth
+        :param grid: if there are prespecified locations to hinge the RBF
+        :param cell_resolution: if 'grid' is 'None', resolution to hinge RBFs
+        :param cell_max_min: if 'grid' is 'None', realm of the RBF field
+        :param X: a sample of lidar locations to use when both 'grid' and 'cell_max_min' are 'None'
+        """
+        self.gamma = gamma
+        if grid is not None:
+            self.grid = grid
+        else:
+            self.grid = self.__calc_grid_auto(cell_resolution, cell_max_min, X)
+        self.nIter = nIter
+        print(' Number of hinge points={}'.format(self.grid.shape[0]))
+
+    def updateGrid(grid):
+        self.grid = grid
+
+    def updateMuSig(mu_sig):
+        self.mu = pt.tensor(mu_sig[:,0], dtype=pt.float32)
+        self.sig = pt.tensor(mu_sig[:,1], dtype=pt.float32)
+
+    def __calc_grid_auto(self, cell_resolution, max_min, X):
+        """
+        :param X: a sample of lidar locations
+        :param cell_resolution: resolution to hinge RBFs as (x_resolution, y_resolution)
+        :param max_min: realm of the RBF field as (x_min, x_max, y_min, y_max, z_min, z_max)
+        :return: numpy array of size (# of RNFs, 2) with grid locations
+        """
+        X = X.numpy()
+
+        if max_min is None:
+            # if 'max_min' is not given, make a boundarary based on X
+            # assume 'X' contains samples from the entire area
+            expansion_coef = 1.2
+            x_min, x_max = expansion_coef*X[:, 0].min(), expansion_coef*X[:, 0].max()
+            y_min, y_max = expansion_coef*X[:, 1].min(), expansion_coef*X[:, 1].max()
+        else:
+            x_min, x_max = max_min[0], max_min[1]
+            y_min, y_max = max_min[2], max_min[3]
+            z_min, z_max = max_min[4], max_min[5]
+
+        xx, yy, zz = np.meshgrid(np.arange(x_min, x_max, cell_resolution[0]), \
+                             np.arange(y_min, y_max, cell_resolution[1]), \
+                             np.arange(z_min, z_max, cell_resolution[2]))
+        grid = np.hstack((xx.ravel()[:, np.newaxis], yy.ravel()[:, np.newaxis], zz.ravel()[:, np.newaxis]))
+
+        return pt.tensor(grid)
+
+    def __sparse_features(self, X):
+        """
+        :param X: inputs of size (N,3)
+        :return: hinged features with intercept of size (N, # of features + 1)
+        """
+        rbf_features = rbf_kernel(X, self.grid, gamma=self.gamma)
+
+        # rbf_features = np.hstack((np.ones(X.shape[0])[:, np.newaxis], rbf_features))
+        return pt.tensor(rbf_features, dtype=pt.float32)
+
+    def __calc_posterior(self, X, y, epsilon, mu0, sig0):
+        """
+        :param X: input features
+        :param y: labels
+        :param epsilon: per dimension local linear parameter
+        :param mu0: mean
+        :param sig0: variance
+        :return: new_mean, new_varaiance
+        """
+        logit_inv = pt.sigmoid(epsilon)
+        lam = 0.5 / epsilon * (logit_inv - 0.5)
+
+        sig = 1/(1/sig0 + 2*pt.sum( (X.t()**2)*lam, dim=1))
+
+        mu = sig*(mu0/sig0 + pt.mm(X.t(), y - 0.5).squeeze())
+
+        return mu, sig
+
+    def fit(self, X, y):
+        """
+        :param X: raw data
+        :param y: labels
+        """
+        X = self.__sparse_features(X)
+        N, D = X.shape[0], X.shape[1]
+
+        self.epsilon = pt.ones(N, dtype=pt.float32)
+        if not hasattr(self, 'mu'):
+            self.mu = pt.zeros(D, dtype=pt.float32)
+            self.sig = 10000 * pt.ones(D, dtype=pt.float32)
+
+        for i in range(self.nIter):
+            print("  Parameter estimation: iter={}".format(i))
+
+            # E-step
+            self.mu, self.sig = self.__calc_posterior(X, y, self.epsilon, self.mu, self.sig)
+
+            # M-step
+            self.epsilon = pt.sqrt(pt.sum((X**2)*self.sig, dim=1) + (X.mm(self.mu.reshape(-1, 1))**2).squeeze())
+        return self.mu, self.sig
+
+    def predict(self, Xq):
+        """
+        :param Xq: raw inquery points
+        :return: mean occupancy (Lapalce approximation)
+        """
+        Xq = self.__sparse_features(Xq)
+
+        mu_a = Xq.mm(self.mu.reshape(-1, 1)).squeeze()
+        sig2_inv_a = pt.sum((Xq ** 2) * self.sig, dim=1)
+        k = 1.0 / pt.sqrt(1 + np.pi * sig2_inv_a / 8)
+
+        return pt.sigmoid(k*mu_a)
+
+    def predictSampling(self, Xq, nSamples=50):
+        """
+        :param Xq: raw inquery points
+        :param nSamples: number of samples to take the average over
+        :return: sample mean and standard deviation of occupancy
+        """
+        Xq = self.__sparse_features(Xq)
+
+        qw = pt.distributions.MultivariateNormal(self.mu, pt.diag(self.sig))
+        w = qw.sample((nSamples,)).t()
+
+        mu_a = Xq.mm(w).squeeze()
+        probs = pt.sigmoid(mu_a)
+
+        mean = pt.std(probs, dim=1).squeeze()
+        std = pt.std(probs, dim=1).squeeze()
+
+        return mean, std
+
+
+class BHM_FULL_PYTORCH():
+    #TODO: double check evrything
+    def __init__(self, gamma=0.075*0.814, grid=None, cell_resolution=(5, 5), cell_max_min=None, X=None):
+        """
+        :param gamma: RBF bandwidth
+        :param grid: if there are prespecified locations to hinge the RBF
+        :param cell_resolution: if 'grid' is 'None', resolution to hinge RBFs
+        :param cell_max_min: if 'grid' is 'None', realm of the RBF field
+        :param X: a sample of lidar locations to use when both 'grid' and 'cell_max_min' are 'None'
+        """
+        self.gamma = gamma
+        if grid is not None:
+            self.grid = grid
+        else:
+            self.grid = self.__calc_grid_auto(cell_resolution, cell_max_min, X)
+        print('D=', self.grid.shape[0])
+
+    def __calc_grid_auto(self, cell_resolution, max_min, X):
+        """
+        :param X: a sample of lidar locations
+        :param cell_resolution: resolution to hinge RBFs as (x_resolution, y_resolution)
+        :param max_min: realm of the RBF field as (x_min, x_max, y_min, y_max)
+        :return: numpy array of size (# of RNFs, 2) with grid locations
+        """
+        X = X.numpy()
+
+        if max_min is None:
+            # if 'max_min' is not given, make a boundarary based on X
+            # assume 'X' contains samples from the entire area
+            expansion_coef = 1.2
+            x_min, x_max = expansion_coef*X[:, 0].min(), expansion_coef*X[:, 0].max()
+            y_min, y_max = expansion_coef*X[:, 1].min(), expansion_coef*X[:, 1].max()
+        else:
+            x_min, x_max = max_min[0], max_min[1]
+            y_min, y_max = max_min[2], max_min[3]
+
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, cell_resolution[0]), \
+                             np.arange(y_min, y_max, cell_resolution[1]))
+        grid = np.hstack((xx.ravel()[:, np.newaxis], yy.ravel()[:, np.newaxis]))
+
+        return pt.tensor(grid)
+
+    def __sparse_features(self, X):
+        """
+        :param X: inputs of size (N,2)
+        :return: hinged features with intercept of size (N, # of features + 1)
+        """
+        rbf_features = rbf_kernel(X, self.grid, gamma=self.gamma)
+        rbf_features = np.hstack((np.ones(X.shape[0])[:, np.newaxis], rbf_features))
+        return pt.tensor(rbf_features, dtype=pt.float32)
+
+    def __lambda(self, epsilon):
+        logit_inv = pt.sigmoid(epsilon)
+        return 0.5 / epsilon * (logit_inv - 0.5)
+
+    def __calc_posterior(self, X, y, epsilon, mu0, sig0_inv):
+        lam = self.__lambda(epsilon)
+        mu0 = mu0.reshape(-1,1)
+
+        #sig = 1/(1/sig0 + 2*pt.sum( (X.t()**2)*lam, dim=1))
+        sig_inv = sig0_inv + 2*pt.mm(X.t() * lam, X)
+        sig = pt.inverse(sig_inv)
+        #sig = pt.diag(1/pt.diag(sig_inv))
+
+        print("sum=", pt.sum(sig_inv.mm(sig)))
+
+        pl.subplot(121)
+        pl.imshow(sig_inv[1:,1:], cmap='jet', interpolation=None); pl.colorbar()
+        pl.subplot(122)
+        pl.imshow(sig[1:,1:], cmap='jet', interpolation=None); pl.colorbar()
+        pl.show()
+
+        #mu = sig*(mu0/sig0 + pt.mm(X.t(), y - 0.5).squeeze())
+        #sig_inv = sig0_inv + 2 * pt.diag(pt.mm(X.t() * lam, X)).squeeze()
+        #mu = pt.mm((1 / sig_inv).reshape(1, -1), (sig_inv * mu0 + pt.mm(X.t(), y - 0.5))).squeeze()
+        mu = sig.mm(sig0_inv.mm(mu0) + pt.mm(X.t(), (y - 0.5)))
+
+        pl.close('all')
+        pl.scatter(self.grid[:,0], self.grid[:,1], c=mu.squeeze()[1:], cmap='jet', s=5, edgecolor='')
+        pl.colorbar()
+        pl.show()
+
+        return mu.squeeze(), sig_inv, sig
+
+    def fit(self, X, y):
+        X = self.__sparse_features(X)
+        N, D = X.shape[0], X.shape[1]
+
+        epsilon = pt.ones(N, dtype=pt.float32)
+        mu = pt.zeros(D, dtype=pt.float32)
+        sig_inv = 0.0001 * pt.eye(D, dtype=pt.float32)
+
+        for i in range(1):
+            print("i=", i)
+
+            # E-step
+            mu, sig_inv, sig = self.__calc_posterior(X, y, epsilon, mu, sig_inv)
+            print('d', mu.shape)
+
+            # M-step
+            XMX = pt.mv(X, mu)**2
+            XSX = pt.sum(pt.mm(X, pt.mm(sig, X.t())), dim=1)
+            print(XMX.shape, XSX.shape)
+            epsilon = pt.sqrt(XMX + XSX) #TODO - Bug
+
+        self.mu, self.sig_inv = mu, sig_inv
+
+    def predict(self, Xq):
+        Xq = self.__sparse_features(Xq)
+
+        sig_diag = 1/pt.diagonal(self.sig_inv)
+
+        mu_a = Xq.mm(self.mu.reshape(-1, 1)).squeeze()
+        sig2_inv_a = pt.sum((Xq ** 2) * sig_diag, dim=1)
+        k = 1.0 / pt.sqrt(1 + np.pi * sig2_inv_a / 8)
+
+        return pt.sigmoid(k*mu_a)
---
+++ BHM Online Learning/e3_carla_video.py
@@ -0,0 +1,250 @@
+"""
+# stacking data and kernels
+# memory - about 3 frames: current and two past ones
+"""
+# import matplotlib.patches as pc
+# import random
+import pandas as pd
+import math
+import os
+import time
+import numpy as np
+import torch as pt
+import matplotlib.pyplot as pl
+from bhmtorch_cpu import BHM2D_PYTORCH
+
+def _initial_value(weight):
+    return (weight[0] == 0) or (weight[1] == 10000)
+
+def _save_new_weights(weights, opt = 'pick_highest_confidence'):
+    weights_dict = {}
+
+    for weight in weights:
+        t, x1, x2, mu, sig = weight
+        kernel = (x1, x2)
+
+        if kernel in weights_dict:
+            # replace
+            if np.size(weights_dict[kernel]) == 2 and _initial_value(weights_dict[kernel]):
+                weights_dict[kernel] = [mu, sig]
+            elif not _initial_value([mu, sig]):
+                weights_dict[kernel] = np.vstack((weights_dict[kernel], [mu, sig]))
+                # take only last two, by now guaranteed at least two elements
+                weights_dict[kernel] = weights_dict[kernel][-2:]
+        else:
+            weights_dict[kernel] = [mu, sig]
+
+    kernel_weight = np.empty([0,4])
+    for kernel in weights_dict:
+        x1, x2 = kernel
+        params = weights_dict[kernel]
+
+        # params should not be empty
+        if np.size(params) > 2:
+            num_param = (np.size(params))/2
+            
+            if opt == 'weight_equally':
+                mu = sum(params[:,0]) / (num_param)
+                sig = math.sqrt(sum([s**2 for s in params[:,1]]) / (num_param**2))
+            elif opt == 'remove_low_confidence':
+                confident_paras_indx = np.log(params[:,1]) <= 3 #3-4 in log-scale seems to be a good value. visualize sig and see.
+                mu = sum(params[confident_paras_indx,0]) / (num_param)
+                sig = math.sqrt(sum([s**2 for s in params[confident_paras_indx,1]]) / (num_param**2))
+            elif opt == 'pick_highest_confidence':
+                hightest_conf_indx = np.argmin(params[:,1])
+                mu = params[hightest_conf_indx, 0]
+                sig = params[hightest_conf_indx, 1]
+            elif opt == 'pick_first_weight':
+                mu = params[0, 0]
+                sig = params[0, 1]
+            kernel_weight = np.vstack((kernel_weight, [x1, x2, mu, sig]))
+        elif np.size(params) > 0:
+            mu, sig = params
+            kernel_weight = np.vstack((kernel_weight, [x1, x2, mu, sig]))
+
+    return kernel_weight
+    
+
+def _hinge_round(x, base):
+    return base * pt.round(x/base)
+
+def _frame_grid(Xt, res):
+    min1, max1 = Xt[:,0].min(), Xt[:,0].max()
+    min2, max2 = Xt[:,1].min(), Xt[:,1].max()
+    grid = [min1, max1, min2, max2]
+
+    grid = [_hinge_round(x, res) for x in grid]
+    grid[0] -= 2*res
+    grid[2] -= 2*res
+    grid[1] += 2*res
+    grid[3] += 2*res
+
+    print('GRID min/max x1, x2 ', grid)
+
+    return grid
+
+def load_parameters(case):
+    parameters = \
+        {
+        'e1_carla': \
+        # ( os.path.abspath('carla_town1_50m_1channel.npz'),
+        (os.path.abspath('../Datasets/carla_town1/carla_town1_lydia_50m_1channel.npz'),
+           (2,2),  #x1 and x2 resolutions for positioning hinge points
+         (-450, 50, -50, 380),  # area to be mapped [x1_min, x1_max, x2_min, x2_max]
+         100,
+         1,  #N/A
+         0.3,  #threshold for filtering data
+         0.7 #gamma: kernel parameter
+          ),
+         }
+
+    return parameters[case]
+
+
+def train_data(filename, frames, out_path, condition):
+    # Settings
+    dtype = pt.float32
+    device = pt.device("cpu")
+
+    # Global variables and hyperparameters
+    QUERY_RESOLUTION = 0.5
+    QUERY_RESOLUTION_FULLMAP = 2*QUERY_RESOLUTION
+    all_parameters = np.asarray([])
+
+    # Read the train and test files
+    fn_train, cell_resolution, cell_max_min, max_distance, _, _, gamma = load_parameters(filename)
+    print('\nReading '+fn_train)
+    dt = np.load(fn_train)
+    Xt = dt['X_train']
+    yt = dt['Y_train'].reshape(-1, 1)
+
+    # Combine x and y data in one big matrix
+    g = np.hstack((Xt, yt))
+
+    # Learn parameters for each frame
+    for framei in frames:
+        print('\nReading frame {}'.format(framei))
+
+        # Filter by frame
+        layer = (g[:,0] == framei)
+        buffer_datapoints = g[layer, :]
+
+        # Extract X and y data from memory buffer for plotting and training
+        f = pt.tensor(buffer_datapoints, dtype=pt.float32)
+        X = f[:, 1:3]
+        y = f[:, 3].reshape(-1, 1)
+
+        # Create kernels for learning based on datapoints max and min
+        frame_limit = _frame_grid(X, cell_resolution[0])
+        xx, yy = np.meshgrid(np.arange(frame_limit[0], frame_limit[1], cell_resolution[0]), \
+                             np.arange(frame_limit[2], frame_limit[3], cell_resolution[1]))
+        model_grid = np.hstack((xx.ravel()[:, np.newaxis], yy.ravel()[:, np.newaxis]))
+
+        # Graph LiDAR points
+        ones_ = np.where(buffer_datapoints[:,3]==1)
+        hit_pts = buffer_datapoints[ones_]
+        zeros_ = np.where(buffer_datapoints[:,3]==0)
+        free_pts = buffer_datapoints[zeros_]
+
+        # IMAGE 1: LiDAR Hit Points and Free Points
+        fig, axs = pl.subplots(1, 4, figsize=(18, 3.5))
+        axs[0].plot(free_pts[:,1], free_pts[:,2],'b.',ms=1.0)
+        axs[0].plot(hit_pts[:,1], hit_pts[:,2], 'r.',ms=1.0)
+        axs[0].set_title('LiDAR Points')
+        fig.suptitle('Frame {}'.format(framei))
+
+        # Learn parameters for the current frame
+        totalTime = 0
+
+        bhm_mdl = BHM2D_PYTORCH(gamma=gamma, grid=model_grid, cell_resolution=cell_resolution, cell_max_min=cell_max_min, X=X, nIter=1)
+
+        t1 = time.time()
+        mu, sig = bhm_mdl.fit(X, y)
+        t2 = time.time()
+        totalTime += (t2-t1)
+        
+        # Record mu, sig parameters for kernels in format: time, x1, x2, mu, sig
+        mu = mu.numpy().reshape(-1,1)
+        sig = sig.numpy().reshape(-1,1)
+        current_parameters = np.hstack((model_grid, np.hstack((mu,sig))))
+        frame_header = np.full((current_parameters.shape[0], 1), framei)
+        current_parameters = np.hstack((frame_header, current_parameters))
+        all_parameters = np.append(all_parameters.reshape(-1, 5), current_parameters, axis=0)
+
+        # Query the model
+        xx, yy= np.meshgrid(np.arange(frame_limit[0]-cell_resolution[0], frame_limit[1], QUERY_RESOLUTION),
+                            np.arange(frame_limit[2]-cell_resolution[1], frame_limit[3], QUERY_RESOLUTION))
+        query_grid = np.hstack((xx.ravel()[:, np.newaxis], yy.ravel()[:, np.newaxis]))
+        Xq = pt.tensor(query_grid, dtype=pt.float32)
+        yq = bhm_mdl.predict(Xq)
+        mean, zq = bhm_mdl.predictSampling(Xq)
+        print(' Total training time={} s'.format(np.round(totalTime, 2)))
+        
+        # IMAGE 2: Instantaneous MEAN map
+        axs[1].scatter(Xq[:, 0], Xq[:, 1], c=yq, cmap='jet', s=5, vmin=0, vmax=1, edgecolors='')
+        pcm =axs[1].get_children()[0] #get the mappable, the 1st and the 2nd are the x and y axes
+        pl.colorbar(pcm, ax=axs[1])
+        axs[1].set_title('Instantaneous map (mean)')
+
+        # IMAGE 3: Instantaneous VARIANCE map
+        axs[2].scatter(Xq[:, 0], Xq[:, 1], c=zq, cmap='jet', s=5, vmin=0, vmax=0.51, edgecolors='')
+        pcm =axs[2].get_children()[0] #get the mappable, the 1st and the 2nd are the x and y axes
+        pl.colorbar(pcm, ax=axs[2])
+        axs[2].set_title('Instantaneous map (variance)')
+        
+        current_data = np.hstack((Xq, yq.reshape(-1,1)))
+        current_data = np.hstack((current_data, zq.reshape(-1,1)))
+        np.save(out_path + 'mean_var_frame{}'.format(framei), current_data)
+
+        # Add in entire map
+        all_data = _save_new_weights(all_parameters, condition)
+        kernel_grid = all_data[:, 0:2]
+        kernel_weights = all_data[:, 2:4]
+
+        # Create the model
+        bhm_mdl = BHM2D_PYTORCH(gamma=gamma, grid=kernel_grid, cell_resolution=cell_resolution, cell_max_min=cell_max_min, X=None, nIter=1, mu_sig = kernel_weights)
+        
+        # Query the model
+        xx, yy= np.meshgrid(np.arange(cell_max_min[0], cell_max_min[1], QUERY_RESOLUTION_FULLMAP),
+                            np.arange(cell_max_min[2], cell_max_min[3], QUERY_RESOLUTION_FULLMAP))
+        all_query_grid = np.hstack((xx.ravel()[:, np.newaxis], yy.ravel()[:, np.newaxis]))
+        
+        Xq_all = pt.tensor(all_query_grid, dtype=dtype)
+
+        print("Grid for entire map initialized")
+
+        t5 = time.time()
+        all_yq = bhm_mdl.predict(Xq_all)
+        t6 = time.time()
+        globalPredTime = (t6-t5)
+        print(' Total global predict time={} s'.format(np.round(globalPredTime, 2)))
+
+        print("\nPlotting for frame {}".format(framei))
+
+        # IMAGE 4 MEAN MAP OF TOWN
+        axs[3].scatter(Xq_all[:, 0], Xq_all[:, 1], c=all_yq, cmap='jet', s=5, vmin=0, vmax=1, edgecolors='')
+        pcm =axs[3].get_children()[0] #get the mappable, the 1st and the 2nd are the x and y axes
+        pl.colorbar(pcm, ax=axs[3])
+        axs[3].set_xlim(cell_max_min[0], cell_max_min[1])
+        axs[3].set_ylim(cell_max_min[2], cell_max_min[3])
+        axs[3].set_title('Map (mean) of the town')
+
+        # Save the figure
+        pl.savefig(os.path.abspath(out_path + 'frame{}.png'.format(framei)))
+        pl.close('all')
+
+        np.save(out_path + 'all_parameters_frame{}_to_{}'.format(FRAMES[0], FRAMES[-1]),all_parameters)
+
+    return all_parameters
+    
+# SET UP
+CONDITIONS = {'we':'weight_equally', 'lc':'remove_low_confidence', 'ph': 'pick_highest_confidence', \
+                'pf': 'pick_first_weight'}
+FRAMES = range(0,4088)
+FILENAME = 'e1_carla'
+OUT_PATH = 'out/'
+
+OPTION = 'ph'
+
+# MAIN
+all_parameters = train_data(FILENAME, FRAMES, OUT_PATH, CONDITIONS[OPTION])
